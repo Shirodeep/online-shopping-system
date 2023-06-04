c66fc797de26782cea468c3560462455a200e950
shirodeep <=shirodeepdahal0987@gmail.com>
2023-06-04 07:00:25 +0545

    registration sucessfull with email and password

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -2,38 +2,39 @@
 <?php
 include_once("./partials/dbconnection.php");
 $upload = 2;
+$errorUpdatingIntoDatabase = false;
 if (isset($_POST['submit'])) {
     $name = $_POST["firstname"] . " " . $_POST["middlename"] . " " . $_POST["lastname"];
     $contact = $_POST["contact"];
     $gender = $_POST["gender"];
     $email = $_POST["email"];
-
-    $forUsernameCheck = "SELECT fullname, contact FROM `useroauth`;";
+    $passwords = $_POST["password"];
+    $forUsernameCheck = "SELECT email, contact FROM useroauth;";
     $isInDb = mysqli_query($db, $forUsernameCheck);
     // to check if user already exists
     while ($checkName = $isInDb->fetch_assoc()) {
-        if ($checkName['contact'] == $contact) {
-            echo "<script>alert('User already exists')</script>";
+        $checkContact = $checkName['contact'];
+        $checkEmail = $checkName['email'];
+        echo "<p>this is the result $checkContact , $checkEmail</p>";
+        
+        if ($checkContact == $contact) {
             $upload = 0;
-            $errorUpdatingIntoDatabase = true;
-            header("Location: ./register.php?q=0");
+            $errorUpdatingIntoDatabase = 0;
+            header("Location: ./register.php?q=$errorUpdatingIntoDatabase");
+
         }
-        if ($checkName['email'] == $email) {
-            echo "<script>alert('User already exists')</script>";
+        if ($checkEmail == $email) {
             $upload = 0;
-            $errorUpdatingIntoDatabase = true;
-            header("Location: ./register.php?q=0");
+            $errorUpdatingIntoDatabase = 0;
+            header("Location: ./register.php?q=$errorUpdatingIntoDatabase");
 
         }
     }
     if ($upload != 0) {
-        $sqlquerry = "INSERT INTO useroauth (fullname, contact, gender) VALUES ('$name', '$contact', '$gender');";
+        $sqlquerry = "INSERT INTO useroauth (fullname, contact, gender, email, passwords) VALUES ('$name', '$contact', '$gender', '$email', '$passwords');";
         $insert = mysqli_query($db, $sqlquerry);
-        $inserted = true;
         header("Location: ./register.php?q=1");
     }
 }
-else{
-    header("Location: ./register.php");
-}
+// header("Location: ./register.php?q=$errorUpdatingIntoDatabase");
 ?>
--- includes/register.php
+++ includes/register.php
@@ -1,39 +1,46 @@
 <body>
     <?php
     include_once('./partials/header.php');
-    include_once("./partials/dbconnection.php");
-     $inserted = $_GET["q"];
-    if ($inserted == 1) {
+    $inserted = $_GET["q"];
+    $p = $_GET["p"];
+    if ($inserted == 1  and $inserted != "") {
         echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+        <strong>Success!!!</strong> User created.
         </button>
         </div>";
-    }
-    else {
+    } elseif($inserted == 0) {
         echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
-        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-      </div>";
+                <strong>Check Your Form Again!</strong> User exists!!!
+                </div>";
     }
+    if ($p == 0 and $p != "") {
+        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+            <strong>Check Your Form Again!</strong> Your password and confirm password do not match.
+          </div>";
+        $p = 1;
+    }
+
+
     ?>
 
     <div class="container mr-5" style="width: 130vh;height:max-content; background-color: whitesmoke;">
         <h1 class="h1 pt-2 ">REGISTER</h1>
         <?php $title = "register"; ?>
-        <form action="/includes/register.inc.php" method="post">
-            <div class="form-row p-3">
-                <div class="col">
+        <form action="/includes/register.inc.php" onsubmit="validate()" method="post">
+            <div class="form-row ">
+                <div class="col p-3">
                     <div class="form-row">
                         <label for="firstname">Firstname</label>&nbsp;
                         <input class="form-control col-xs-3" type="text" name="firstname" required>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col p-3">
                     <div class="form-row">
                         <label for="middlename">Middlename</label>&nbsp;
                         <input class="form-control col-xs-3" type="text" name="middlename" required>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col p-3">
                     <div class="form-row">
                         <label for="lastname">Lastname</label>&nbsp;
                         <input class="form-control col-xs-3" type="text" name="lastname" required>
@@ -48,8 +55,14 @@
             </div>
             <div class="row p-3">
                 <div class="col">
-                    <label for="contact">Contact</label> &nbsp;
-                    <input class="form-control" type="text" name="contact" pattern="(\+977)?[9][7-9]\d{8}" required>
+                    <label for="password">Password</label> &nbsp;
+                    <input class="form-control" type="password" name="password" id="password" required>
+                </div>
+            </div>
+            <div class="row p-3">
+                <div class="col">
+                    <label for="confirmPassword">Confirm password</label> &nbsp;
+                    <input class="form-control" type="password" name="confirmPassword" id="confirmPassword" required>
                 </div>
             </div>
             <div class="row p-3">
@@ -59,6 +72,16 @@
                     <label class="form-check-label" for="male">Male</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <input class="form-check form-check-inline" type="radio" name="gender" id="female" value="female" required>
                     <label class="form-check-label" for="female">Female</label>
+                </div>
+            </div>
+            <div class="row p-3">
+                <div class="col">
+                    <label for="contact">Contact</label> &nbsp;
+                    <div class="row">
+                        <input class="col-sm-2 col-lg-1" style="border-radius: 15%;" type="text" placeholder="+977" readonly>
+                        <input class="col form-control" type="text" name="contact" id="contact" pattern="(\+977)?[9][7-9]\d{8}" required>
+
+                    </div>
                 </div>
             </div>
             <input class="btn btn-outline-secondary text-dark" type="submit" name="submit" value="Submit">
