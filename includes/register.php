4b4cce7ea8e6d6debd54d4629b414a6eaf3ff152
shirodeep <=shirodeepdahal0987@gmail.com>
2023-06-12 02:42:29 +0545

    dismissable alert button added

--- includes/register.php
+++ includes/register.php
@@ -4,101 +4,111 @@
     $passwordType = "password";
     $inserted = $_GET["q"];
     $p = $_GET["p"];
-    if ($inserted == 1  and $inserted != "") {
-        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-        <strong>Success!!!</strong> User created.
-        </button>
-        </div>";
+    if ($inserted == 1  and $inserted != "") { ?>
+        <div class="row alert alert-warning alert-dismissible fade show" role="alert">
+            <strong class="col-11">SUCCESS USER CREATED!</strong>
+            <button type="button" class="col btn btn-outline-success" data-bs-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        <?php
     } elseif ($inserted == 0 and $inserted != "") {
-        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
-                <strong>Check Your Form Again!</strong> User exists!!!
-                </div>";
-    }
-    if ($p == 0 and $p != "") {
-        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
-            <strong>Check Your Form Again!</strong> Your password and confirm password do not match.
-          </div>";
-        $p = 1;
-    }
-
-
-    ?>
-
-    <div class="container mr-5" style="width: 130vh;height:max-content; background-color: whitesmoke;">
-        <h1 class="h1 pt-2 ">REGISTER</h1>
-        <?php $title = "register"; ?>
-        <form action="/includes/register.inc.php" onsubmit="return validate()" method="post">
-            <div class="form-row ">
-                <div class="col p-3">
-                    <div class="form-row">
-                        <label for="firstname">Firstname</label>&nbsp;
-                        <input class="form-control col-xs-3" type="text" name="firstname" required>
-                    </div>
-                </div>
-                <div class="col p-3">
-                    <div class="form-row">
-                        <label for="middlename">Middlename</label>&nbsp;
-                        <input class="form-control col-xs-3" type="text" name="middlename" required>
-                    </div>
-                </div>
-                <div class="col p-3">
-                    <div class="form-row">
-                        <label for="lastname">Lastname</label>&nbsp;
-                        <input class="form-control col-xs-3" type="text" name="lastname" required>
-                    </div>
-                </div>
-            </div>
-            <div class="row p-3">
-                <div class="col">
-                    <label for="email">Email</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <input class="form-control" type="email" name="email" pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" required>
-                </div>
-            </div>
-            <div class="row p-3">
-                <div class="col">
-                    <label for="password">Password</label> &nbsp;
-                    <div class="row">
-                        <input class="col form-control" type="password" name="password" id="password" required>
-                        <button class="col-lg-1 btn btn-secondary col-sm-1"type="button" onclick="showandhide('password')">
-                    <i class="bi bi-eye" style="font-size: 3vh;"></i>
+        ?>
+            <div class="row alert alert-warning alert-dismissible fade show" role="alert">
+                <strong class="col-11">Check Your Form. USER EXISTS!!!</strong>
+                <button type="button" class="col btn btn-outline-warning" data-bs-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            <?php
+        }
+        if ($p == 0 and $p != "") {
+            ?>
+                <div class="row alert alert-warning alert-dismissible fade show" role="alert">
+                    <strong class="col-11">Check Your Form!</strong>  PASSWORD AND CONFIRM PASSWORD DO NOT MATCH!!!
+                    <button type="button" class="col btn btn-outline-warning" data-bs-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
                     </button>
+                <?php
+                $p = 1;
+            }
 
-                    </div>
-                </div>
-            </div>
-            <div class="row p-3">
-                <div class="col">
-                    <label for="confirmPassword">Confirm password</label> &nbsp;
-                    <div class="row">
-                        <input class=" col form-control" type="password" name="confirmPassword" id="confirmPassword" required>
-                        <button class="col-lg-1  btn btn-secondary col-sm-1" type="button" onclick="showandhide('confirmPassword')"><i class="bi bi-eye" style="font-size: 3vh;"></i></button>
 
-                    </div>
-                </div>
-            </div>
-            <div class="row p-3">
-                <div class="col">
-                    <label class="form-check-label" for="Gender">Gender</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <input class="form-check form-check-inline" type="radio" name="gender" value="male" id="male" required>
-                    <label class="form-check-label" for="male">Male</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <input class="form-check form-check-inline" type="radio" name="gender" id="female" value="female" required>
-                    <label class="form-check-label" for="female">Female</label>
-                </div>
-            </div>
-            <div class="row p-3">
-                <div class="col">
-                    <label for="contact">Contact</label> &nbsp;
-                    <div class="row">
-                        <input class="col-sm-2 col-lg-1" style="border-radius: 15%;" type="text" placeholder="+977" readonly>
-                        <input class="col form-control" type="text" name="contact" id="contact" pattern="(\+977)?[9][7-9]\d{8}" required>
+                ?>
 
-                    </div>
+                <div class="container mr-5" style="width: 130vh;height:max-content; background-color: whitesmoke;">
+                    <h1 class="h1 pt-2 ">REGISTER</h1>
+                    <?php $title = "register"; ?>
+                    <form action="/includes/register.inc.php" onsubmit="return validate()" method="post">
+                        <div class="form-row ">
+                            <div class="col p-3">
+                                <div class="form-row">
+                                    <label for="firstname">Firstname</label>&nbsp;
+                                    <input class="form-control col-xs-3" type="text" name="firstname" required>
+                                </div>
+                            </div>
+                            <div class="col p-3">
+                                <div class="form-row">
+                                    <label for="middlename">Middlename</label>&nbsp;
+                                    <input class="form-control col-xs-3" type="text" name="middlename" required>
+                                </div>
+                            </div>
+                            <div class="col p-3">
+                                <div class="form-row">
+                                    <label for="lastname">Lastname</label>&nbsp;
+                                    <input class="form-control col-xs-3" type="text" name="lastname" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row p-3">
+                            <div class="col">
+                                <label for="email">Email</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input class="form-control" type="email" name="email" pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" required>
+                            </div>
+                        </div>
+                        <div class="row p-3">
+                            <div class="col">
+                                <label for="password">Password</label> &nbsp;
+                                <div class="row">
+                                    <input class="col form-control" type="password" name="password" id="password" required>
+                                    <button class="col-lg-1 btn btn-secondary col-sm-1" type="button" onclick="showandhide('password')">
+                                        <i class="bi bi-eye" style="font-size: 3vh;"></i>
+                                    </button>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row p-3">
+                            <div class="col">
+                                <label for="confirmPassword">Confirm password</label> &nbsp;
+                                <div class="row">
+                                    <input class=" col form-control" type="password" name="confirmPassword" id="confirmPassword" required>
+                                    <button class="col-lg-1  btn btn-secondary col-sm-1" type="button" onclick="showandhide('confirmPassword')"><i class="bi bi-eye" style="font-size: 3vh;"></i></button>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row p-3">
+                            <div class="col">
+                                <label class="form-check-label" for="Gender">Gender</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input class="form-check form-check-inline" type="radio" name="gender" value="male" id="male" required>
+                                <label class="form-check-label" for="male">Male</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input class="form-check form-check-inline" type="radio" name="gender" id="female" value="female" required>
+                                <label class="form-check-label" for="female">Female</label>
+                            </div>
+                        </div>
+                        <div class="row p-3">
+                            <div class="col">
+                                <label for="contact">Contact</label> &nbsp;
+                                <div class="row">
+                                    <input class="col-sm-2 col-lg-1" style="border-radius: 15%;" type="text" placeholder="+977" readonly>
+                                    <input class="col form-control" type="text" name="contact" id="contact" pattern="(\+977)?[9][7-9]\d{8}" required>
+
+                                </div>
+                            </div>
+                        </div>
+                        <input class="btn btn-outline-secondary text-dark" type="submit" name="submit" value="Submit">
+                    </form>
                 </div>
-            </div>
-            <input class="btn btn-outline-secondary text-dark" type="submit" name="submit" value="Submit">
-        </form>
-    </div>
-    <?php include_once('./partials/footer.php'); ?>
+                <?php include_once('./partials/footer.php'); ?>
 </body>
 
 <script>
@@ -110,11 +120,12 @@
             return false;
         }
     }
-    function showandhide(data){
+
+    function showandhide(data) {
         let showPassword = document.getElementById(data);
-        if(showPassword.type == "password"){
+        if (showPassword.type == "password") {
             showPassword.type = "text";
-        }else{
+        } else {
             showPassword.type = "password";
         }
     }
