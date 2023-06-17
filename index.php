14fc4fa37dfe006bec1da7e3a11935b4744e0e37
shirodeep <shirodeepdahal0987@gmail.com>
2023-06-17 13:38:06 +0545

    only some items are shown in home page

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <html>
 <?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 session_start();
 $title = "Thikxa";
-$uname = $_SESSION["uname"];
 include_once("./includes/partials/header.php");
 include_once("./includes/partials/dbconnection.php");
 ?>
@@ -14,30 +16,50 @@ include_once("./includes/partials/dbconnection.php");
             </button>
         </div>
     <?php } ?>
-    <?php
-    $itemsQrr = "SELECT * from items;";
-    $items = mysqli_query($db, $itemsQrr);
-    while ($row = mysqli_fetch_assoc($items)) {
-        $name = $row['itemname'];
-        $rating = $row['itemrating'];
-        $price = $row['itemprice'];
-        echo '
-        <h1>Our Collections</h1>
-        <div class="container-fluid p-4 d-flex flex-row flex-wrap">
-            <a class="items p-1 m-1" href="#" style="color:black; text-decoration: none;border: 1px solid gray; border-radius: 15px;">
-            <img src="./public/images/thumb.png" height="150" width="150" alt="image">
-            <div class="row">
-                <div class="col">' . $name . '</div>
+    <div class="container-fluid p-3" id="collections">
+        <div class="row ">    
+            <h1>Our Collections</h1>
+            <div class="d-flex flex-row-reverse" style="height: max-content;z-index: 999;">
+                <div class=""><a href="./itemdisplay.php"><button class="btn btn-warning">Show more</button></a></div>
             </div>
-            <div class="row">
-                <div class="col">' . $rating . '</div>
-            </div>
-            <div class="row">
-                <div class="col">रु ' . $price . '</div>
-            </div>
-            </a>   
-        </div>';
-    }
-    include("./includes/partials/footer.php"); ?>
+        </div>
+        <div class=" d-flex flex-wrap" style="height: max-content;">
+        <?php
+                $itemsQrr = "SELECT * from items;";
+                $items = mysqli_query($db, $itemsQrr);
+                $rowCount = 7;
+                while ($row = mysqli_fetch_assoc($items)) {
+                    $iname = $row["iname"];
+                    $iprice = $row["iprice"];
+                    $idescription = $row["idescription"];
+                    $iquantity = $row["iquantity"];
+                    $iimage = $row["iimage"];
+                    $iimage = "/public/images/uploaded/" . $iimage;
+                    $iid = $row["iid"];
+                    if ($rowCount >= 1) {
+                        echo '
+                    <a class="items mx-2 my-2 p-2 m-1" href="./includes/itemsdescription.php?id=' . $iid . '" style="color:black; text-decoration: none;border: 1px solid gray; border-radius: 15px;height:max-content;width:30vh">
+                    <img src="'.$iimage.'" height="150" width="150">
+                    <div class="row p-1">
+                    <div class="col"> '. $iname .' </div>
+                    </div>
+                    <div class="row p-1">
+                    <div class="col">रु ' . $iprice . '</div>
+                    </div>
+                    <div class="row p-1">
+                    <div class="col"></div>
+                    </div>
+                    </a>';
+                    }
+                    else{
+                        break;
+                    }
+                    $rowCount--;
+                } ?>
+        </div>
+    </div>
+    </div>
+    <?php include("./includes/partials/footer.php"); ?>
 </body>
+
 </html>
