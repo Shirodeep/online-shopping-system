e214cddf147e9d27c5ff7e8a90b14a5c1d00201d
shirodeep <shirodeepdahal0987@gmail.com>
2023-06-17 13:30:02 +0545

    items can be clicked to addto cart or buy

--- includes/itemsdescription.php
+++ includes/itemsdescription.php
@@ -1,8 +1,8 @@
 <?php
-include_once("./partials/header.php");
-include_once("./partials/dbconnection.php");
 session_start();
 $uname = $_SESSION["uname"];
+include_once("./partials/dbconnection.php");
+include_once("./partials/header.php");
 $itemid = $_GET["id"];
 $qrr = "SELECT * FROM items WHERE iid=$itemid;";
 $result = mysqli_query($db, $qrr);
@@ -16,61 +16,77 @@ $iimage = $result["iimage"];
 $iimage = "/public/images/uploaded/" . $iimage;
 $iid = $result["iid"];
 ?>
+<?php
+if ($_GET["isOnCart"] == 1 and $_GET["isOnCart"] != "") { ?>
+    <div class="row alert alert-warning alert-dismissible fade show" role="alert">
+        <strong class="col-11">ITEM ALREADY ON CART</strong>
+        <button type="button" class="col btn btn-outline-success" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+<?php
+}
+if ($_GET["isOnCart"] == 0 and $_GET["isOnCart"] != "") { ?>
+    <div class="row alert alert-success alert-dismissible fade show" role="alert">
+        <strong class="col-11">ADDED TO CART!!!</strong>
+        <button type="button" class="col btn btn-outline-success" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+<?php
+}
+?>
 <div class="container my-5">
     <div class="row">
         <div class="col-lg-6  col-sm-6"><img src="<?php echo $iimage ?>" height="100%" width="100%"></div>
         <div class="col-lg-6  col-sm-6 ">
-            <div class="row p-3 ">
-                <div class="col">
-                    <h2><?php echo $iname ?></h2>
+            <form action="./addtocart.php">
+                <input type="text" value="<?php echo $iid ?>" name="itemid" hidden>
+                <input type="text" value="<?php echo $iname ?>" name="itemname" hidden>
+                <input type="text" value="<?php echo $iprice ?>" name="price" hidden>
+
+                <input type="text" value="<?php
+                                            $encryptionKey = "LALA!@#";
+                                            $encrypted = openssl_encrypt($iimage, "AES-128-CTR", $encryptionKey);
+                                            echo $encrypted;
+                                            ?>" name="xxyyzz" hidden>
+
+                <div class="row p-3 ">
+                    <div class="col">
+                        <h2><?php echo $iname ?></h2>
+                    </div>
                 </div>
-            </div>
-            <div class="row p-2">
-                <div class="col">
-                    <h2><?php echo "Rs " . $iprice ?></h2>
+                <div class="row p-2">
+                    <div class="col">
+                        <h2><?php echo "Rs " . $iprice ?></h2>
+                    </div>
                 </div>
-            </div>
-            <div class="row p-2">
-                <div class="col">
-                    <spam class="text-secondary">Quantity: </spam>
-                    <button class="btn btn-primary" onclick="addDelete('-');">-</button>
-                    <INPUT type="text" id="addQuantity" value="1" readonly style="width: 5vh; text-align: center">
-                    <button class="btn btn-primary" onclick="addDelete('+');">+</button>
+                <div class="row p-2">
+                    <div class="col">
+                        <spam class="text-secondary">Quantity: </spam>
+                        <button class="btn btn-primary" type="button" onclick="addDelete('-', 'addQuantity');">-</button>
+                        <input type="text" id="addQuantity" name="quantity" value="1" readonly style="width: 5vh; text-align: center">
+                        <button class="btn btn-primary" type="button" onclick="addDelete('+', 'addQuantity');">+</button>
+                    </div>
                 </div>
-            </div>
-            <div class="row p-2">
-                <h6 class=" col p-2">Available Items : <?php echo $iquantity ?></h6>
-            </div>
-            <div class="row p-2 d-flex justify-content-between">
-                <button class="col-5 btn btn-outline-warning" onclick="return checkSigned()">Buy Now</button>
-                <button class="col-5 btn btn-outline-success" onclick="return checkSigned()">Add to Cart</button>
-            </div>
+                <div class="row p-2">
+                    <h6 class=" col p-2">Available Items : <?php echo $iquantity ?></h6>
+                </div>
+                <div class="row-sm p-2 d-flex justify-content-between">
+                    <div class="col-2"></div>
+                    <a class="col-3" href="./buy.php?"><button class=" btn btn-outline-warning" name="buy" type="button" style="width: 100%">Buy Now</button></a>
+                    <a class="col-3"><button class=" btn btn-outline-success" type="submit" name="cart" value="c<?php echo mt_rand(323121, 927188) ?>" style="width: 100%">Add to Cart</button></a>
+                    <div class="col-2"></div>
+                </div>
+            </form>
         </div>
     </div>
     <div class="row bg-secondary text-warning text-center " style="height: 50vh;padding-top: 15vh">
-        <h1>Rating + FeedBack Available Soon</h1>
+        <h1>Rating + FeedBack Available Soon...</h1>
     </div>
+
 </div>
 <?php include_once("./partials/footer.php"); ?>
 
-<script>
-    function addDelete(compare) {
-        let max_quantity = Number('<?php echo $iquantity ?>');
-        let x = document.getElementById("addQuantity");
-        if (compare == '-' && x.value > 1) {
-            x.value--;
-        }
-        if (compare == '+' && x.value < 5) {
-            x.value++;
-        }
-    }
-
-    function checkSigned() {
-        let isSessionSet = '<?php echo $uname ?>';
-        if(isSessionSet != ""){
-            alert("go to shopping");
-            return true;
-        }
-        return false;
-    }
+<script src="../js/alljs.js">
 </script>
